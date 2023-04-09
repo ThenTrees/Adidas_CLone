b0dcad4360376ddd23407489f42656c51b49d007
ThienTri <thientri.trank17@gmail.com>
2023-04-10 02:04:33 +0700

    update hover product

---
+++ js/option.js
@@ -0,0 +1,8 @@
+// let options = document.querySelectorAll(".options");
+let options = document.querySelectorAll(".option");
+let icon = options.getClassByTagName("i");
+
+for (let o of options) {
+    o.addEventListener("click", () => {
+    });
+}
