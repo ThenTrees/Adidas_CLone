72b168d0c38f9d17961cb6625f9ab872ed9caf8f
ThienTri <thientri.trank17@gmail.com>
2023-04-10 20:24:28 +0700

    hoàn thành sư kiện click ở options

--- js/option.js
+++ js/option.js
@@ -1,8 +1,29 @@
 // let options = document.querySelectorAll(".options");
-let options = document.querySelectorAll(".option");
-let icon = options.getClassByTagName("i");
+let options = document.querySelector(".options");
+const items = options.querySelectorAll("p");
+// const widthItems = options.querySelectorAll("p").offsetWidth;
 
-for (let o of options) {
-    o.addEventListener("click", () => {
+// Xử lý sự kiện "click" cho mỗi phần tử
+items.forEach(function (item, icon) {
+    item.addEventListener("click", function () {
+        // Xoá class "active" khỏi tất cả các phần tử con
+        items.forEach(function (item) {
+            item.classList.remove("option-p__setColor");
+        });
+
+        // Thêm class "active" cho phần tử được nhấp vào
+        this.classList.add("option-p__setColor");
+        let w = document.querySelector(".option-p__setColor").offsetWidth;
+        let icon = options.querySelector(".fa-icon-arrow-op");
+        console.log(w);
+        if (w === 173) {
+            icon.style.transform = `translateX(0px)`;
+        } else if (w === 216) {
+            icon.style.transform = `translateX(230px)`;
+        } else if (w === 88) {
+            icon.style.transform = `translateX(490px)`;
+        } else if (w === 390) {
+            icon.style.transform = `translateX(615px)`;
+        }
     });
-}
+});
