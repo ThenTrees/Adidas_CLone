45dfc62c6ada3b834d7d2c0f20414587c5c8cc61
ThienTri <thientri.trank17@gmail.com>
2023-04-12 11:41:34 +0700

    thêm chức năng click chuyển sản phẩm

---
+++ js/product_click.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $(".list_product_1").slick({
+        autoplay: true,
+        autoplaySpeed: 1000,
+
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        arrows: true,
+        prevArrow:
+            "<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+        nextArrow:
+            "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+    });
+});
